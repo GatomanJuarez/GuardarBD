1672859b4dae59dbd390752b6281e3df12fc5353
Jesus Antonio JD <juarez_devora_ja@hotmail.com>
2017-03-23 08:45:23 -0800

    Add files via upload

---
+++ GuardadoImagenTest/GuardadoImagenTest/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace GuardadoImagenTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.pBlimagen = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.btnInsertar = new System.Windows.Forms.ToolStripMenuItem();
+            this.verToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pBlimagen)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(49, 12);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(100, 20);
+            this.txtnombre.TabIndex = 0;
+            // 
+            // pBlimagen
+            // 
+            this.pBlimagen.Location = new System.Drawing.Point(12, 38);
+            this.pBlimagen.Name = "pBlimagen";
+            this.pBlimagen.Size = new System.Drawing.Size(225, 169);
+            this.pBlimagen.TabIndex = 1;
+            this.pBlimagen.TabStop = false;
+            this.pBlimagen.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnInsertar,
+            this.verToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(9, 228);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(101, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(58, 20);
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // verToolStripMenuItem
+            // 
+            this.verToolStripMenuItem.Name = "verToolStripMenuItem";
+            this.verToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.verToolStripMenuItem.Text = "Ver";
+            this.verToolStripMenuItem.Click += new System.EventHandler(this.verToolStripMenuItem_Click);
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(277, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(349, 169);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 261);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pBlimagen);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pBlimagen)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.PictureBox pBlimagen;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem btnInsertar;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.ToolStripMenuItem verToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ GuardadoImagenTest/GuardadoImagenTest/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Data;
+
+namespace GuardadoImagenTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        MySqlConnection Cnn = new MySqlConnection("server=localhost;database=Human;Uid=root;pwd=hola");
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            Cnn.Open();
+            if (txtnombre.Text == "")
+            {
+                MessageBox.Show("Ningun campo debe estar vacio", "Error");
+            }
+            else
+            {
+                try
+                {
+                    MySqlCommand cmd = new MySqlCommand("Insert into test(nombre,imagen)values('"+txtnombre.Text+"',@imagen) ",Cnn);
+                    MemoryStream ms = new MemoryStream();
+
+                    pBlimagen.Image.Save(ms, ImageFormat.Jpeg);
+                    byte[] aByte = ms.ToArray();
+                    cmd.Parameters.AddWithValue("imagen", aByte);
+                    int i = 0;
+                    i = cmd.ExecuteNonQuery();
+                    if (i > 0)
+                    {
+                        MessageBox.Show("Datos guardados correctamente", "Guardar");
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            Cnn.Close();
+        }
+
+        private void verToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MySqlCommand comando = new MySqlCommand("Select Imagen from test where Id=1",Cnn);
+            MySqlDataAdapter dp = new MySqlDataAdapter(comando);
+            DataSet ds = new DataSet("Imagen");
+            dp.Fill(ds, "Imagen");
+            byte[] DAtos = new byte[0];
+            DataRow DR;
+            DR = ds.Tables["Imagen"].Rows[0];
+
+            DAtos = (byte[])DR["Imagen"];
+
+            System.IO.MemoryStream ms = new System.IO.MemoryStream(DAtos);
+            pictureBox1.Image = System.Drawing.Bitmap.FromStream(ms);
+        }
+
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+            ofd.Filter = "Archivo de Imagen | *.jpg| Archivo PNG|*.png| Todos los archivos|*.*";
+            DialogResult resultado = ofd.ShowDialog();
+
+            if (resultado == DialogResult.OK)
+            {
+                pBlimagen.Image = Image.FromFile(ofd.FileName);
+            }
+        }  
+
+    }
+}
